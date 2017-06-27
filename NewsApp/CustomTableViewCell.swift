e3cd0da8a28b711d9553e0ad8ae1ef7773bf72f3
hahahahahanananana <mocohanamocohana@i.softbank.jp>
2017-06-27 20:37:30 +0900

    20170627

---
+++ NewsApp/CustomTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTableViewCell.swift
+//  NewsApp
+//
+//  Created by hahahahahanananana on 2017/06/27.
+//  Copyright © 2017年 片岡. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var myImageview: UIImageView!
+    @IBOutlet var myTitleLabel: UILabel!
+    @IBOutlet var myDedcriptionLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+  
+}
--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -9,28 +9,59 @@
 import UIKit
 
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet var table:UITableView!
+    let image1 = UIImage(named:"cow01.jpg")
+    let image2 = UIImage(named:"shutterstock_128724608-1-690x491.jpg")
+    let image3 = UIImage(named:"s_0043.jpg")
+    let image4 = UIImage(named:"jidori06.jpg")
+    
+    let imageNames = ["shutterstock_128724608-1-690x491.jpg","cow01.jpg","s_0043.jpg","jidori06.jpg"]
+    
+    let imageTitles = ["牛","牛肉","鶏","鶏肉"]
+    let imageDecriptions: [String] = [
+        "見てくる牛",
+        "牛の肉",
+        "横向きの鶏",
+        "鶏の肉"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         table.dataSource = self
+        table.delegate = self
+        self.table.register(UINib(nibName: "CustomTableViewCell",bundle: nil), forCellReuseIdentifier: "customCell")
+        
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 10
+        return imageNames.count
     }
-    func tableView(_ tableView: UITableView, cellForRowAt  indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell? .textLabel?.text = "aaa"
-        return cell!
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell",for: indexPath) as! CustomTableViewCell
+        
+        cell.myImageview.image = UIImage(named: imageNames[indexPath.row])
+        cell.myTitleLabel.text = imageTitles[indexPath.row]
+        cell.myDedcriptionLabel.text = imageDecriptions[indexPath.row]
+        
+        return cell
     }
+    
+    
 }
 
 
